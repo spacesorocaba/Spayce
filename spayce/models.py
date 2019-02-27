ea372f2847ec8817bf9243b1c3fd682f8e9d946b
raphael <raphael.costa@birmind.com.br>
2019-02-27 20:39:02 -0300

    add fields to models

--- spayce/models.py
+++ spayce/models.py
@@ -8,6 +8,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     preco = models.FloatField(default=0.0)
+    ativo = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
@@ -18,8 +19,9 @@ class Pedido(models.Model):
                                    on_delete=models.CASCADE)
     item = models.ForeignKey(Produto, related_name='produto',
                              on_delete=models.CASCADE)
-    quantidade = models.IntegerField(default=1)
+    quantidade = models.IntegerField(null=False, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+    pago = models.BooleanField(default=False)
 
     def __str__(self):
         return self.item.nome
