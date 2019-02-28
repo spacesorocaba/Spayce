711b8c278ea40153c22de7c27c085c55d896a398
raphael <raphael.costa@birmind.com.br>
2019-02-28 20:44:59 -0300

    fix - bug quando adiciona uma ordem nova

--- core/settings.py
+++ core/settings.py
@@ -129,4 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+MEDIA_ROOT = '/var/www/media/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/var/www/static/'
 STATIC_URL = '/static/'
