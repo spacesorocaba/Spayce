6c5e78ee96b9b23ca007614006533dc060e191ad
thiagolopes <thiagolopes@protonmail.com>
2019-04-03 20:22:08 -0300

    update url

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from spayce.views import (import_csv, orderdetail, orderlist, productdetail,
-                          productlist, productview, spacerview)
+from spayce.views import (import_csv, import_products_csv, orderdetail,
+                          orderlist, productdetail, productlist, productview,
+                          spacerview)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
